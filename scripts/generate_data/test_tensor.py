a5b26a0e700cb89cb8b38578ebdd48be5f8cc2fd
Hzz <2267730671@qq.com>
2025-03-10 16:08:44 +0800

    test_tensor

--- scripts/generate_data/test_tensor.py
+++ scripts/generate_data/test_tensor.py
@@ -1,4 +1,23 @@
 import torch
 
-def is_empty_tensor(x):
-    return x.numel() == 0
+
+def load_and_check(file_path):
+    try:
+        # 加载数据
+        loaded_data = torch.load(file_path)
+
+        # 提取张量（假设文件存储单个张量）
+        if not isinstance(loaded_data, torch.Tensor):
+            raise ValueError("文件内容非张量类型")
+
+        # 空张量检测
+        is_empty = (loaded_data.numel() == 0) or (0 in loaded_data.shape)
+        print(f"张量形状：{loaded_data.shape}，是否为空：{is_empty}")
+
+        return loaded_data
+    except FileNotFoundError:
+        print("错误：文件路径无效")
+    except Exception as e:
+        print(f"数据加载异常：{str(e)}")
+for i in range(0,499):
+    load_and_check(f"/home/ylc/mpd-public/scripts/generate_data/logs/generate_trajectories_2025-03-07_18-28-43/env_id___EnvSpheres3D/robot_id___RobotPanda/{i}/trajs-collision.pt")
